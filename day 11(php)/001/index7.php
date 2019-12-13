a57721303635fc04b75c2631455f060fd84b87d6
ITPlus <nguyenbac2398@gmail.com>
2019-12-13 18:45:15 +0700

    111

---
+++ day 11(php)/001/index7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Biến PHP</h1>
+    <h1>Toán tử . là toán tử nối chuỗi</h1>
+
+    <?php
+        $x = 5;
+        $y = 3;
+
+        echo "<br> Tổng " . ($x + $y);
+        echo "<br> Hiệu " . ($x - $y);
+
+        $ho = "Nguyễn Văn ";
+        $ten = "B";
+        echo  "<br> $ho" . "" . "$ten";
+    ?>
+</body>
+</html>
